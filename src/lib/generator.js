eb73262f2897fe4485b8f7c522c64d3c039896c4
Jack Harvey <jackrrv@gmail.com>
2019-05-06 21:05:49 +0100

    Add text generator

---
+++ src/lib/generator.js
@@ -0,0 +1,106 @@
+const fs = require('fs');
+const { resolve } = require('path');
+const { promisify } = require('util');
+const { lowestOrder, EOLToken, totalToken } = require('../consts');
+
+const readFile = promisify(fs.readFile);
+
+const CHARACTER_LIMIT = 280;
+
+async function generator(user, order) {
+    if (!user) {
+        throw new Error('User not specified');
+    }
+
+    const orders = [...Array(order + 1)]
+        .map((_, i) => i)
+        .filter(order => order >= lowestOrder);
+
+    const chains = await readChains(user, orders);
+
+    const initialWords = generateInitialWords(chains, orders);
+
+    const text = completeTextGeneration(chains[order], initialWords, order);
+
+    const EOLRegex = new RegExp(` ${EOLToken}\\s?`, 'g');
+
+    return text
+        .join(' ')
+        .replace(EOLRegex, '. ');
+}
+
+function readChains(user, orders) {
+    const readPath = resolve(`${__dirname}/../chains`);
+    const fileReads = orders.map(order => readFile(`${readPath}/${user}-order${order}.json`, 'utf8'));
+
+    return Promise.all(fileReads)
+        .then(chains => chains.reduce((chainCatalogue, chain, i) => ({
+            ...chainCatalogue,
+            [i + lowestOrder]: JSON.parse(chain),
+        }), {}));
+}
+
+function completeTextGeneration(chain, text, order) {
+    const relevantHistory = text.slice((-1 * order) + 1);
+    const chainLeaf = getChainLeaf(chain, relevantHistory);
+
+    if (!chainLeaf) {
+        return text;
+    }
+
+    const nextWord = selectNextWord(chainLeaf);
+
+    const nextSentence = [
+        ...text,
+        nextWord,
+    ];
+
+    if (nextWord === EOLToken) {
+        const tweet = nextSentence.join(' ');
+        if (tweet.length > CHARACTER_LIMIT) {
+            const [, truncated] = tweet.match(`((.*)${EOLToken}).+`);
+            const initialWords = nextSentence.slice(0, order - 1);
+            return truncated ? nextSentence : completeTextGeneration(chain, initialWords, order);
+        }
+    }
+
+    return completeTextGeneration(chain, nextSentence, order);
+}
+
+const generateInitialWords = (chains, orders) =>
+    orders
+        .reduce((path, order) => {
+            const chain = chains[order];
+            const chainLeaf = getChainLeaf(chain[EOLToken], path);
+
+            return [
+                ...path,
+                selectNextWord(chainLeaf),
+            ];
+        }, []);
+
+const getChainLeaf = (chain, path) =>
+    path.reduce((accumulator, word) =>
+        accumulator ? accumulator[word] : null
+    , chain);
+
+function selectNextWord(chainLeaf) {
+    const total = chainLeaf[totalToken];
+    const percentile = Math.ceil(Math.random() * total) / total;
+
+    const nextWord = Object.entries(chainLeaf).find(([key, value]) =>
+        key !== totalToken && value === percentile
+    );
+
+    // if percentile is not an exact match (i.e. percentiles in the chain can
+    // jump more than 1 / total if it occurs more than once) then we must try
+    // again with a new percentile
+    return nextWord ? nextWord[0] : selectNextWord(chainLeaf);
+}
+
+module.exports = {
+    generator,
+    completeTextGeneration,
+    readChains,
+    generateInitialWords,
+};
